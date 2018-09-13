0cd58a282052d1f9b13c3d362325f446ff8a9cd2
thiagonespereira <tlpereir@gmail.com>
2018-09-13 11:49:26 -0300

    Login page created with error handling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,39 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { firebaseConfig } from '../config/firebase.config';
+import { AuthService } from '../services/auth.service';
+import { NgxErrorsModule } from '@ultimate/ngxerrors';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig.fire),
+    NgxErrorsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireAuth,
+    AuthService
   ]
 })
 export class AppModule {}
---
+++ src/config/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    fire: {
+        apiKey: "AIzaSyBy3cy5Gg2AobT09F6yonHmh9jG9fFyCMw",
+        authDomain: "simplefirebaseauth-48635.firebaseapp.com",
+        databaseURL: "https://simplefirebaseauth-48635.firebaseio.com",
+        projectId: "simplefirebaseauth-48635",
+        storageBucket: "simplefirebaseauth-48635.appspot.com",
+        messagingSenderId: "1019822428621"
+    }
+};
